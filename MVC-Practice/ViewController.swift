aba4c98933cb55f588567700d44dae6c68561df8
Reon0429-cat <oonishireon110@gmail.com>
2021-09-15 02:04:29 +0900

    first commit

---
+++ MVC-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Practice
+//
+//  Created by 大西玲音 on 2021/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
